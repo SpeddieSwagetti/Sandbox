78f052127365f93ba3458518fd0005bccc523dc0
SpeddieSwagetti <mitchjmarks@outlook.com>
2019-03-22 20:18:25 +1000

    Add password check program

--- password_entry.py
+++ password_entry.py
@@ -1,1 +1,19 @@
 """Mitchell Marks"""
+
+while True:
+    try:
+        MIN_PASSWORD_LENGTH = int(input("Specify a minimum password length: "))
+        break
+    except ValueError or NameError:
+        print("Enter a valid length, you simpleton")
+while True:
+    try:
+        password = input("Please enter a valid password {} characters long)".format(MIN_PASSWORD_LENGTH))
+        password_length = len(password)
+    except ValueError:
+        print("You managed to screw this up? -_-")
+    if password_length < MIN_PASSWORD_LENGTH:
+        print("You managed to screw this up? -_-")
+    else:
+        break
+print("*" * password_length)
